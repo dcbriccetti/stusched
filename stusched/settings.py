f16d315ce4c8498cec614e26dbf1580ad9079496
Dave Briccetti <daveb@davebsoft.com>
2016-07-24 13:58:31 -0700

    Add whitenoise middleware

--- stusched/settings.py
+++ stusched/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stusched.urls'
