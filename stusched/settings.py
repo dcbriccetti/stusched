e26bd4c122c824c40d58a0ace392fe14a5c358b4
Dave Briccetti <daveb@davebsoft.com>
2016-07-29 20:12:10 -0700

    Turn off USE_TZ

--- stusched/settings.py
+++ stusched/settings.py
@@ -100,7 +100,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/Los_Angeles'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
