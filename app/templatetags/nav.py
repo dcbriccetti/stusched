acadc287a654c074fec5df34b743ac150705ecd1
Dave Briccetti <daveb@davebsoft.com>
2016-08-13 18:02:36 -0700

    Simplify active_nav tag code

--- app/templatetags/nav.py
+++ app/templatetags/nav.py
@@ -1,11 +1,9 @@
 from django.core.urlresolvers import resolve
 from django.template import Library
+
 register = Library()
 
 
 @register.simple_tag
 def active_nav(request, url):
-    url_name = resolve(request.path).url_name
-    if url_name == url:
-        return "active"
-    return ""
+    return 'active' if resolve(request.path).url_name == url else ''
